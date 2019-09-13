96db60654ec551996a4c6bc454af364a61d726fa
james-pope <jamespope@billericak12.com>
2019-09-13 11:03:33 -0400

    uhhhhhh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	int number = (int)(Math.random() * (100-1)+1) + 1;
 	System.out.println(number);
+	System.out.println("goodbye");
     }
 }
