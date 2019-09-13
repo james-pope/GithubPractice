d350ca23cacc0d88a33d9ca33da112f4561dca30
james-pope <jamespope@billericak12.com>
2019-09-13 10:58:22 -0400

    uhhhhhh

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int number = (int)(Math.random() * (100-1)+1) + 1;
+	System.out.println(number);
+    }
+}
