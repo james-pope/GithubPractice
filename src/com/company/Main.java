02fe9934e9db8305f635b05f28d2f62c347a4634
james-pope <jamespope@billericak12.com>
2019-09-13 12:07:51 -0400

    Hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,13 @@ package com.company;
 public class Main {
 
 	public static void main(String[] args) {
-		//double x = Math.random();  // random number
-		//	}between [0,1]
-		//double  x = (int)(Math.random() * 10);
-		int x = (int) (Math.random() * (89 - 48) + 48);
-		System.out.println(x);
+		int x = (int)(Math.random() * 10);
+		int y = (int)(Math.random() * (6-1)+1)+1;
+		int z = (int)(Math.random() * 2);
+		int dice1 = (int)(Math.random() * (6-1)+ 1)+ 1;
+		int dice2 = (int)(Math.random() * (6-1)+ 1)+ 1;
+		int c = (int)(Math.random() * (100-65)+ 1)+65;
+		double d = (Math.random() * 10);
+		System.out.println(d);
 	}
 }
